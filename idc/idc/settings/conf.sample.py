319f1cf5404952f643a9f0a4c4189cec2967ef85
Dheerendra Rathor <dheeru.rathor14@gmail.com>
2016-02-02 15:12:56 +0000

    Bootstrapped IDC project

---
+++ idc/idc/settings/conf.sample.py
@@ -0,0 +1,21 @@
+"""
+Configuration file for settings
+"""
+
+SECRET_KEY = '<SECRET KEY>'
+
+ALLOWED_HOSTS = ['*']
+
+DB_NAME = 'idc_profile'
+
+DB_USERNAME = 'idc_profile'
+
+DB_PASSWORD = 'idc_profile'
+
+DB_HOST_NAME = 'localhost'
+
+DB_PORT = '5432'
+
+ADMINS_EMAIL_LIST = [
+    # ('Name', 'email@example.com'),
+]
