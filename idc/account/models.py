09421e2b962beccca9779332fab3f3144350286e
bijoy <bijoysingh693@gmail.com>
2016-02-21 01:56:00 +0530

    Adding UI for a single blog

--- idc/account/models.py
+++ idc/account/models.py
@@ -3,10 +3,14 @@ from django.db import models
 
 
 class UserProfile(models.Model):
-    user = models.ForeignKey(User, related_name='user_profile')
+    user = models.OneToOneField(User, related_name='user_profile')
     nickname = models.CharField(max_length=32)
+    picture = models.ImageField(null=True, blank=True)
 
     # Social Accounts
-    facebook = models.URLField()
-    twitter = models.URLField()
-    behance = models.URLField()
+    facebook = models.URLField(null=True, blank=True)
+    twitter = models.URLField(null=True, blank=True)
+    google = models.URLField(null=True, blank=True)
+    blogger = models.URLField(null=True, blank=True)
+    homepage = models.URLField(null=True, blank=True)
+    behance = models.URLField(null=True, blank=True)
